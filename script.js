2ae2a2c6ec9b70acdaf9fa51df44a64e7a882164
Eyyüp Demirkıran <eypdmrkn6@gmail.com>
2020-12-19 03:56:51 +0000

    Odev Teslimi

--- script.js
+++ script.js
@@ -1,55 +1,69 @@
-function Animal(name){
+class newAnimal{
+  constructor(name, animalURL ){
     this.name = name;
-}
-
-Animal.prototype.action = function(){
-  document.getElementById(this.actionSoundName).play();
+    this.animalURL = amimalURL;
+  }
   
-}
-
-Animal.prototype.putInTheDocument = function(){
+  action(){
+    document.getElementById(this.actionSoundName).play();
+  }
+  
+  putInTheDocument(){
     var petsTable = document.getElementById("petsTable");
     var petTR = document.createElement("tr");
-  
+
     var petNameTD = document.createElement("td");
     petNameTD.textContent = this.name;
     petTR.appendChild(petNameTD);
-  
+
     var petLegsTD = document.createElement("td");
     petLegsTD.textContent = this.legs;
     petTR.appendChild(petLegsTD);
-  
+
     var petActionTD = document.createElement("td");
     var petActionTDButton = document.createElement("button");
     petActionTDButton.textContent = this.actionText;
     petActionTD.appendChild(petActionTDButton);
     petTR.appendChild(petActionTD);
-    
+
     petActionTDButton.onclick = this.action.bind(this);
     petsTable.querySelector("tbody").appendChild(petTR)
+    
+    /* 
+    function change(){
+      imageURL = document.getElementById("idImg");
+      imageURL.src= this.idImg;
+    }
+    const ImageURL = document.querySelector("idImg"); 
+    ImageURL.addEventListener("click", change);
+  }
+  */
+
+  }
 }
 
-
-function Cat(name){
-    Animal.call(this, name);
+class Cat extends newAnimal {
+  constructor(name, animalURL){
+    super(name, animalURL);
     this.legs = 4;
     this.actionText = "Meoow"
     this.actionSoundName = "meow"
+    this.idImg ="catId";
+  }
 }
 
-Cat.prototype = Animal.prototype;
-
-function Monkey(name){
-    Animal.call(this, name);
+class Monkey extends newAnimal{
+  constructor(name,animalURL){
+    super(name,animalURL);
     this.legs = 2;
     this.actionText = "Scream";
     this.actionSoundName = "scream";
+    this.idImg = "monkeyId";
+  }
 }
 
-Monkey.prototype = Animal.prototype;
+const objMonkey = new Monkey("Charlie");
+objMonkey.putInTheDocument();
 
-var Mila = new Cat("Mila");
-Mila.putInTheDocument();
-
-var Charlie = new Monkey("Charlie");
-Charlie.putInTheDocument();
+const objCat = new Cat("Mila");
+objCat.putInTheDocument();
